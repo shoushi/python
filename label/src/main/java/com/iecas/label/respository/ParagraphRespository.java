1494849c1c01b9dffa51658e65eaa5e8f419578b
shoushi <729904932@qq.com>
2018-04-02 10:37:45 +0800

    delete .idea dir

--- label/src/main/java/com/iecas/label/respository/ParagraphRespository.java
+++
@@ -1,7 +0,0 @@
-package com.iecas.label.respository;
-
-import com.iecas.label.model.Paragraph;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ParagraphRespository extends MongoRepository<Paragraph,Long> {
-}
